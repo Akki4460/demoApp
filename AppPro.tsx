d8b8834150f25ad75ea6cfba8d853d7167a44688
akkibhosale4460@gmail.com <akkibhosale4460@gmail.com>
2024-01-30 20:44:55 +0530

    Use of "StyleSheet" and "useColorScheme"

--- AppPro.tsx
+++ AppPro.tsx
@@ -1,9 +1,47 @@
 import React from "react";
 
-import {} from "react-native"
+import {
+    Text,
+    View,
+    StyleSheet,
+    useColorScheme,
+    SafeAreaView,
+    Button,
+    Alert
+} from "react-native"
 
-function appPro(){
+// This provides some features to write less buggy code
+function AppPro() : JSX.Element{
+
+    const isDarkMode = useColorScheme() === 'dark'
+
+    return(
+        <SafeAreaView>
+            <View style={Styles.container}>
+                {/* Concluding IF dark mode is on change text to light and vise-versa */}
+                <Text style={isDarkMode?Styles.whiteTxt:Styles.darkTxt}>AppPro.tsx</Text>
+                {/* Button just for demo with event onPress */}
+                <Button title="Hi" onPress={()=>Alert.alert("Hiii!!!")}></Button>
+            </View>
+        </SafeAreaView>
+    )
 
 }
 
-export default appPro
+
+    const Styles = StyleSheet.create({
+        container : {
+            backgroundColor:"red",
+        },
+
+        whiteTxt:{
+            color: "#FFFFFF"
+        },
+
+        darkTxt:{
+            color: "#000000"
+        }
+    })
+
+
+export default AppPro
--- index.js
+++ index.js
@@ -4,9 +4,8 @@
 
 import {AppRegistry} from 'react-native';
 // import App from './App';
-import App from './AppPro';
 import {name as appName} from './app.json';
-import appPro from './AppPro';
+import AppPro from './AppPro';
 
 // AppRegistry.registerComponent(appName, () => App);
-AppRegistry.registerComponent(appName, () => appPro);
+AppRegistry.registerComponent(appName, () => AppPro);
