76b1905239a649301834d26a3fe0d48d80ef31cb
akkibhosale4460@gmail.com <akkibhosale4460@gmail.com>
2024-01-30 20:07:40 +0530

    Creating and Replacing app.tsx with appPro.tsx

--- index.js
+++ index.js
@@ -3,7 +3,10 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+// import App from './App';
+import App from './AppPro';
 import {name as appName} from './app.json';
+import appPro from './AppPro';
 
-AppRegistry.registerComponent(appName, () => App);
+// AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => appPro);
