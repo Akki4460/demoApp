dcd7735e7d58c29efc56201eb547135b2478eb07
akkibhosale4460@gmail.com <akkibhosale4460@gmail.com>
2024-02-09 19:24:41 +0530

    Introduction to DeepLinking + TouchableOpacity

---
+++ components/ActionCards.tsx
@@ -0,0 +1,83 @@
+import { Image, Linking, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import React from 'react'
+
+const ActionCards = () => {
+
+
+    function openLink(webLink: string){
+        Linking.openURL(webLink)
+    }
+
+  return (
+    <View>
+      <Text>ActionCards</Text>
+      <View style={[styles.card, styles.cardElevated]}>
+          <Text style={styles.cardTitle}>Lorem, ipsum</Text>
+        <Image
+            source={{
+                uri:'https://images.unsplash.com/photo-1704419278774-fa691a032632?q=80&w=1975&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+            }} 
+            style={styles.cardImage}
+            />
+        <View style={styles.cardBody}>
+            <Text style={styles.cardLabel}>Lorem ipsum dolor sit amet.</Text>
+            <Text numberOfLines={3} style={styles.cardDescription}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Harum sint ullam sed ipsa placeat at similique provident ex eius praesentium! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nesciunt cupiditate eos distinctio repellat voluptatibus amet, culpa nam repudiandae eius quam.</Text>
+            <View style={styles.cardFooter}>
+                <TouchableOpacity onPress={() => openLink('https://reactnative.dev/docs/touchableopacity')}>
+                    <Text>Read More...</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+      </View>
+    </View>
+  )
+}
+
+export default ActionCards
+
+const styles = StyleSheet.create({
+    card:{
+        width:320,
+        height:360,
+        borderRadius:6,
+        marginVertical:12,
+        marginHorizontal:6,
+        
+    },
+    cardElevated:{
+        backgroundColor:'#ffffff',
+        elevation:4
+    },
+    cardImage:{
+        height:200,
+        marginBottom:8,
+        borderTopLeftRadius:5,
+        borderTopRightRadius:5
+    },
+    cardBody:{
+        flex:1,
+        flexGrow:1,
+        paddingHorizontal:12,
+    },
+    cardTitle:{
+        color:'#000000',
+        fontSize:22,
+        fontWeight:'bold',
+        marginBottom:6,
+        paddingHorizontal:8
+    },
+    cardLabel:{
+        color:'#000000',
+        fontSize:16,
+        fontWeight:'500',
+        marginBottom:4
+    },
+    cardDescription:{
+        color:'#000000',
+        marginBottom:15,
+        flexShrink:1,
+        textAlign:'justify'
+    },
+    cardFooter:{
+    }
+})
--- components/AppDemo.tsx
+++ components/AppDemo.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import FlatCards from './FlatCards'
 import ElevatedCards from './ElevatedCards'
 import FancyCards from './FancyCards'
+import ActionCards from './ActionCards'
 
 const AppDemo = () => {
   return (
@@ -11,6 +12,7 @@ const AppDemo = () => {
             <FlatCards />
             <ElevatedCards />
             <FancyCards />
+            <ActionCards />
         </ScrollView>
     </SafeAreaView>
   )
